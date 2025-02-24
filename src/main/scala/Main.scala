f8d7f8995b4931e9bccf331697b39099dc73ad7f
isakcarrero <isak.carrero@gmail.com>
2025-02-24 15:10:27 +0200

    Sidebar added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,12 @@
+
+import scalafx.scene.layout.*
+import scalafx.scene.*
+import scalafx.scene.control.{Alert, Button, ChoiceDialog, Label, Menu, MenuBar, MenuItem, ScrollPane, Slider, SplitPane, Tab, TabPane, TableView, TextField, TextInputDialog, Tooltip}
+import scalafx.stage.{Modality, Stage}
+import scalafx.scene.{Node, Scene, control}
 import scalafx.application.JFXApp3
-import scalafx.scene.layout._
-import scalafx.scene._
-import scalafx.scene.control.{Menu, MenuBar, MenuItem}
+import scalafx.geometry.{HPos, Insets, VPos}
+import scalafx.scene.layout.{BorderPane, HBox}
 
 
 object Main extends JFXApp3:
@@ -13,21 +18,30 @@ object Main extends JFXApp3:
       width = 950
       height = 700
 
+
     val rootPane = BorderPane()
 
-    val scene = Scene(parent = rootPane)
+    val scene = Scene(
+      parent = rootPane,
+      /**fill = Color.rgb(63,63,63)**/
+    )
+
     stage.scene = scene
+    /**********************************************************************************************
+    /**The menubar**/
+    **********************************************************************************************/
 
-    /**The menu bar**/
     val menu = new MenuBar
-
+    /**menu.setStyle("-fx-background-color: #3f3f3f")**/
     val menuFiles = Menu("File")
     val newFile = new MenuItem("New")
     val importFile = new MenuItem("Import")
     val exportFile = new MenuItem("Export")
     menuFiles.items = List(newFile, importFile, exportFile)
 
-    val createPortfolio = Menu("Create Portfolio")
+    val portfolio = Menu("Portfolio")
+    val createPortfolio = new MenuItem("Create portfolio")
+    portfolio.items = List(createPortfolio)
 
     val help = Menu("Help")
     val addStock = new MenuItem("Adding a stock")
@@ -35,9 +49,26 @@ object Main extends JFXApp3:
     val addChart = new MenuItem("Adding a chart")
     help.items = List(addStock, removeStock, addChart)
 
-    menu.menus = List(menuFiles, createPortfolio, help)
-
+    menu.menus = List(menuFiles, portfolio, help)
     rootPane.top = menu
+
+
+    /**********************************************************************************************
+    /**Menubar methods**/
+    **********************************************************************************************/
+
+    /**********************************************************************************************
+    /**Event handling**/
+    **********************************************************************************************/
+
+    /**********************************************************************************************
+    /**Sidebar**/
+    **********************************************************************************************/
+    val sidebar = new VBox()
+    sidebar.setMinWidth(200)
+    sidebar.setStyle("-fx-background-color: #ececec")
+    rootPane.left = sidebar
+
 
 
   end start
