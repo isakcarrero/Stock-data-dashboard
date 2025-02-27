6cd1ff61c64674e036e2765e9fd1178a87da8e27
isakcarrero <isak.carrero@gmail.com>
2025-02-27 21:42:29 +0200

    Added newPortfolio, a method that first shows textinputfield when create portfolio addae and then creates a portfolio square in the sidepar. Portfolio lacks functionality.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,18 @@
 
 import scalafx.scene.layout.*
+import scalafx.Includes.*
 import scalafx.scene.*
 import scalafx.scene.control.{Alert, Button, ChoiceDialog, Label, Menu, MenuBar, MenuItem, ScrollPane, Slider, SplitPane, Tab, TabPane, TableView, TextField, TextInputDialog, Tooltip}
-import scalafx.stage.{Modality, Stage}
+import scalafx.stage.{Modality, Popup, Stage}
 import scalafx.scene.{Node, Scene, control}
 import scalafx.application.JFXApp3
-import scalafx.geometry.{HPos, Insets, VPos}
+import scalafx.event.ActionEvent
+import scalafx.geometry.{HPos, Insets, Pos, VPos}
 import scalafx.scene.layout.{BorderPane, HBox}
+import scalafx.Includes.eventClosureWrapperWithParam
+import scalafx.event.EventIncludes.eventClosureWrapperWithParam
+import scalafx.scene.shape.Rectangle
+
 
 
 object Main extends JFXApp3:
@@ -20,19 +26,19 @@ object Main extends JFXApp3:
 
 
     val rootPane = BorderPane()
-
-    val scene = Scene(
-      parent = rootPane,
-      /**fill = Color.rgb(63,63,63)**/
-    )
+    val scene = Scene(parent = rootPane)
 
     stage.scene = scene
+
+
+
+
     /**********************************************************************************************
-    /**The menubar**/
+    /**The menubar and the different options (File, Portfolio, Help), as well as their suboptions**/
     **********************************************************************************************/
 
+
     val menu = new MenuBar
-    /**menu.setStyle("-fx-background-color: #3f3f3f")**/
     val menuFiles = Menu("File")
     val newFile = new MenuItem("New")
     val importFile = new MenuItem("Import")
@@ -52,22 +58,60 @@ object Main extends JFXApp3:
     menu.menus = List(menuFiles, portfolio, help)
     rootPane.top = menu
 
+    /**********************************************************************************************
+    /**Sidebar**/
+    **********************************************************************************************/
+    val sidebar = new VBox()
+    sidebar.setPrefWidth(200)
+    sidebar.setMaxWidth(200)
+    sidebar.setStyle("-fx-background-color: #ececec")
+    rootPane.left = sidebar
 
     /**********************************************************************************************
     /**Menubar methods**/
     **********************************************************************************************/
 
+    /** For creating a new fortfolio **/
+    def newPortfolio(): Unit =
+      val dialog = new TextInputDialog():
+        title = "Name Your Portfolio"
+        headerText = "Enter the name of your new portfolio:"
+        contentText = "Portfolio Name:"
+      val result = dialog.showAndWait()
+
+      result match
+        case Some(name) =>
+          val portfolioLabel = new Label(name)
+          portfolioLabel.style = "-fx-padding: 5px;"
+
+          val addButton = new Button("+"):
+            style = "-fx-font-size: 10px; -fx-padding: 5px 7px;"
+
+      /** Need to fix the spacing between the objects **/
+          val hbox = new HBox:
+            maxWidth = sidebar.width.value
+            children = Seq(portfolioLabel, addButton)
+            alignment = Pos.CenterRight
+
+          val rectangle = new Rectangle:
+            width = sidebar.width.value
+            height = 30
+            style = "-fx-fill: white; -fx-stroke: darkgray; -fx-stroke-width: 1px;"
+
+          val stackPane = new StackPane:
+            children = Seq(rectangle, hbox)
+            alignment = Pos.CenterLeft
+            style = "-fx-padding: 5px 0px 0px 0px;"
+
+          sidebar.children.add(stackPane)
+        case None =>
+          println("Portfolio creation cancelled.")
     /**********************************************************************************************
     /**Event handling**/
     **********************************************************************************************/
 
-    /**********************************************************************************************
-    /**Sidebar**/
-    **********************************************************************************************/
-    val sidebar = new VBox()
-    sidebar.setMinWidth(200)
-    sidebar.setStyle("-fx-background-color: #ececec")
-    rootPane.left = sidebar
+    createPortfolio.onAction = (e: ActionEvent) => newPortfolio()
+
 
 
 
